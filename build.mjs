97bed622d8c05b2a440e6e1c29fa9e6666659479
Max Milton <max@wearegenki.com>
2021-05-08 07:27:46 +1000

    Rethink bundles:
    - Split reconcilers into their own bundles
    - Use .js for bundles (for better compat with other tools)
    - Add fields to pkg

--- build.mjs
+++ build.mjs
@@ -5,25 +5,22 @@ import esbuild from 'esbuild';
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-/** @param {Error?} err */
+/** @param {Error|null} err */
 function handleErr(err) {
   if (err) throw err;
 }
 
-// New Tab app
 esbuild
   .build({
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.mjs',
-    platform: 'browser',
-    // target: ['chrome88'],
-    format: 'esm',
-    define: {
-      'process.env.NODE_ENV': JSON.stringify(mode),
-    },
-    // banner: { js: '"use strict";' },
+    entryPoints: [
+      'src/index.ts',
+      'src/reconcile/keyed.ts',
+      'src/reconcile/reconcile.ts',
+      'src/reconcile/reuse-nodes.ts',
+    ],
+    outdir: 'dist',
+    platform: 'neutral',
     bundle: true,
-    // minify: !dev,
     sourcemap: true,
     watch: dev,
     logLevel: 'debug',
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export * from './compile';
 export * from './events';
-// export * from './reconcile/keyed';
-// export * from './reconcile/reconcile';
-export * from './reconcile/reuse-nodes';
 export * from './types';
