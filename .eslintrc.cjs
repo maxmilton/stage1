7769a35e8d1d1eabea014b411b456dd53dc54d2e
Max Milton <max@wearegenki.com>
2021-05-11 14:10:06 +1000

    Better lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,15 @@
 const OFF = 0;
+const WARN = 1;
 
-/** @type {import('eslint').Linter.Config} */
+// eslint-disable-next-line max-len
+/** @type {import('eslint').Linter.Config & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     extraFileExtensions: ['.mjs', '.cjs'],
-    project: ['./tsconfig.json', './test/tsconfig.json'],
-  },
-  env: {
-    node: true,
+    project: ['./test/tsconfig.json'],
   },
   extends: [
     'eslint:recommended',
@@ -21,16 +20,15 @@ module.exports = {
   ],
   rules: {
     'import/prefer-default-export': OFF,
+    // useful for compact and memory efficient code... but be careful!
+    'no-cond-assign': OFF,
+    // can be used for efficient code (if used safely!)
+    'no-param-reassign': WARN,
+    'no-plusplus': OFF,
     'no-restricted-syntax': OFF,
+    // useful for compact and memory efficient code... but be careful!
+    'no-return-assign': OFF,
     // used in synthetic event handler names
     'no-underscore-dangle': OFF,
   },
-  overrides: [
-    {
-      files: ['.eslintrc.cjs'],
-      parserOptions: {
-        createDefaultProgram: true,
-      },
-    },
-  ],
 };
