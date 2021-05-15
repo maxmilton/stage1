5f141f2a6bb02a6aaef4c3a5047400bbc22a5d94
Max Milton <max@wearegenki.com>
2021-05-16 08:18:16 +1000

    Ignore some lint errors

--- src/events.ts
+++ src/events.ts
@@ -6,9 +6,12 @@ const nativeToSyntheticEvent = (event: Event, name: string) => {
   let dom = event.target as Node | null;
 
   while (dom !== null) {
+    // @ts-expect-error - string indexing dom is unavoidable
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const eventHandler = dom[eventKey];
 
     if (eventHandler) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       eventHandler(event);
       return;
     }
