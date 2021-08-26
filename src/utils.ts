5138a4b53fe9d8b3a23bd290cecea67c7b5f491c
Max Milton <max@wearegenki.com>
2021-08-26 13:12:25 +1000

    Add "prepend" DOM util

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,5 @@ export const create = <K extends keyof HTMLElementTagNameMap>(
   tagName: K,
 ): HTMLElementTagNameMap[K] => document.createElement(tagName);
 export const append = <T extends Node>(node: T, parent: Node): T => parent.appendChild(node);
+// eslint-disable-next-line max-len
+export const prepend = <T extends Node>(node: T, parent: Node): T => parent.insertBefore(node, parent.firstChild);
