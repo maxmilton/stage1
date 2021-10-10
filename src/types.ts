819cf8092da6586a97c370d1a8cc16fe2b00bb46
Max Milton <max@wearegenki.com>
2021-10-10 15:26:58 +1000

    Use most correct node type

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export interface RefNodes {
   [key: string]: Node;
 }
 
-export interface S1Node extends Node {
+export interface S1Node extends Node, ChildNode {
   _refs: Ref[];
   collect<T extends RefNodes = RefNodes>(node: Node): T;
 }
