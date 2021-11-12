1aa20174f5322b5d2521a173a7105a06e0b4625d
Max Milton <max@wearegenki.com>
2021-11-12 14:36:39 +1000

    Add experimental hh compiler function

--- src/compile.ts
+++ src/compile.ts
@@ -107,3 +107,23 @@ export const h = (
 
   return node;
 };
+
+// TODO: If this variant proves to be viable, it should be renamed to `h`
+export const hh = (template: string): S1Node => {
+  // Compatible template literal minifier is mandatory for production consumers!
+  compilerTemplate.innerHTML = process.env.NODE_ENV === 'production'
+    ? template
+    : template
+      // to get the correct first node
+      .trim()
+      // faster genPath and cleaner test snapshots
+      .replace(/\n\s+/g, '\n')
+      // remove whitespace around ref tags in Text nodes
+      .replace(/>\s+#(\w+)\s+</gm, '>#$1<');
+
+  const node = compilerTemplate.content.firstChild as S1Node;
+  node._refs = genPath(node);
+  node.collect = collect;
+
+  return node;
+};
