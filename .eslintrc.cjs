650c36f32bb2283fb6f05a7d3f84949958f8d0ba
Max Milton <max@happyhackers.xyz>
2023-03-20 12:38:56 +1100

    chore: Update VS Codeand eslint for code formatting with just prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,14 @@ const WARN = 1;
 const ERROR = 2;
 
 // TODO: Types
-// eslint-disable-next-line max-len
 // /** @type {import('eslint/lib/shared/types').ConfigData & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    extraFileExtensions: ['.mjs', '.cjs'],
     project: ['./test/tsconfig.json'],
+    tsconfigRootDir: __dirname,
   },
   extends: [
     'eslint:recommended',
@@ -20,16 +19,15 @@ module.exports = {
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
+  plugins: ['prettier'],
   rules: {
-    // TODO: Fix issues then remove this rule
-    '@typescript-eslint/naming-convention': WARN,
-
     '@typescript-eslint/explicit-module-boundary-types': ERROR,
-    'default-param-last': WARN,
-    'import/prefer-default-export': OFF,
     // void return can be used for efficient code (if used safely!)
     'consistent-return': WARN,
+    'default-param-last': WARN,
+    'import/prefer-default-export': OFF,
     // useful for compact and memory efficient code... but be careful!
     'no-cond-assign': OFF,
     // can be used for efficient code (if used safely!)
@@ -40,5 +38,6 @@ module.exports = {
     'no-return-assign': OFF,
     // used in synthetic event handler names
     'no-underscore-dangle': OFF,
+    'prettier/prettier': WARN,
   },
 };
