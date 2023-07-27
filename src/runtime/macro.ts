1b0dff9bf915f54630acfe27d83682da4638f89a
Max Milton <max@happyhackers.xyz>
2023-07-27 10:58:03 +1000

    feat: Add single root check to compile macro

--- src/runtime/macro.ts
+++ src/runtime/macro.ts
@@ -1,12 +1,12 @@
 export interface CompileOptions {
   /**
-   * Keep HTML comments in output HTML.
+   * Whether to keep HTML comments in output HTML.
    * @default false
    */
   keepComments?: boolean;
   /**
-   * Keep spaces adjacent to tags in output HTML. When keepSpaces is `false`,
-   * `<div> x </div>` becomes `<div>x</div>`.
+   * Whether to keep spaces adjacent to tags in output HTML. When keepSpaces
+   * is false, `<div> x </div>` becomes `<div>x</div>`.
    * @default false
    */
   keepSpaces?: boolean;
@@ -16,10 +16,13 @@ export interface CompileOptions {
  * Bun macro which compiles a template string at build-time into a format that
  * can be used by the runtime.
  * @param template - HTML template string.
- * @param keepComments - Whether to keep HTML comments in the output.
+ * @param options - Compile options.
  */
 export async function compile(
   template: string,
+  // FIXME: Actually using objects in macro arguments is broken in bun.
+  //  ↳ https://github.com/oven-sh/bun/issues/3832
+  //  ↳ Add to documentation if it's not resolved by the time we write proper docs
   { keepComments, keepSpaces }: CompileOptions = {},
   // @ts-expect-error - Bun macros always result in synchronous inlined data.
 ): { html: string; k: readonly string[]; d: readonly number[] } {
@@ -28,9 +31,25 @@ export async function compile(
   const d: number[] = [];
   let distance = 0;
   let whitespaceSensitiveBlock = false;
+  let root: boolean | undefined;
 
   rewriter.on('*', {
     element(node) {
+      if (!root) {
+        if (root === undefined) {
+          root = true;
+          node.onEndTag(() => {
+            root = false;
+          });
+        } else {
+          // eslint-disable-next-line no-console
+          console.error(
+            'Expected template to have a single root element:',
+            template,
+          );
+        }
+      }
+
       if (node.tagName === 'pre' || node.tagName === 'code') {
         whitespaceSensitiveBlock = true;
         node.onEndTag(() => {
@@ -48,10 +67,11 @@ export async function compile(
       }
       distance++;
     },
+    // This text handler is invoked twice for each Text node: first with the
+    // actual text, then with an empty last chunk. This behavior stems from
+    // the fact that the Response provided to HTMLRewriter.transform() is not
+    // streamed; otherwise, there could be multiple chunks before the last one.
     text(chunk) {
-      // Since the response given to HTMLRewriter.transform() is not streamed,
-      // this text handler is called once with the acual text, and then again
-      // with an empty last chunk.
       if (!chunk.lastInTextNode) {
         const text = chunk.text.trim();
         if (!text) {
@@ -75,6 +95,7 @@ export async function compile(
     },
     comments(node) {
       if (keepComments) {
+        // TODO: Support comments as node refs.
         distance++;
       } else {
         node.remove();
