794ae45427f04621c426fdad7e6b5f77c757da21
Max Milton <max@happyhackers.xyz>
2024-03-20 13:29:40 +1100

    test: Add workaround for happy-dom bug

--- test/setup.ts
+++ test/setup.ts
@@ -50,6 +50,14 @@ function setupMocks(): void {
   global.performance.mark = noop;
   // @ts-expect-error - noop stub
   global.performance.measure = noop;
+
+  // TODO: Remove once happy-dom supports `HTMLLIElement` again.
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (window.HTMLLIElement) {
+    throw new TypeError('HTMLLIElement already defined');
+  }
+  // @ts-expect-error - mock element
+  window.HTMLLIElement = window.HTMLElement;
 }
 
 export async function reset(): Promise<void> {
