3a69abdfd0d87e357c71685ff3e9371f1b0c5d94
Max Milton <max@happyhackers.xyz>
2024-07-06 15:53:33 +0900

    test: Simplify test tooling

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testMatch: 'test/e2e/**/*.spec.ts',
+  forbidOnly: !!process.env.CI,
+  use: {
+    acceptDownloads: false,
+    contextOptions: { strictSelectors: true },
+    locale: 'en-US',
+    // offline: true,
+    timezoneId: 'UTC',
+  },
 });
