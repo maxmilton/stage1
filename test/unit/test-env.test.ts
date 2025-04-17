54d60ed08039998f041d9a8cef1bcffab20f7e07
Max Milton <max@happyhackers.xyz>
2025-04-17 20:44:47 +0900

    test: Add test env tests

---
+++ test/unit/test-env.test.ts
@@ -0,0 +1,44 @@
+/**
+ * @file Validate the test environment is setup correctly.
+ */
+
+import { describe, expect, test } from 'bun:test';
+
+describe('globals', () => {
+  // @see https://github.com/maxmilton/test-utils/blob/master/src/dom.ts#L34
+  const globals = [
+    'window',
+    'document',
+    'console',
+    'navigator',
+    'location',
+    'history',
+    'localStorage',
+    'fetch',
+    'setTimeout',
+    'clearTimeout',
+    'setInterval',
+    'clearInterval',
+    'queueMicrotask',
+    'requestAnimationFrame',
+    'cancelAnimationFrame',
+    'postMessage',
+    'dispatchEvent',
+    'addEventListener',
+    'removeEventListener',
+    'DocumentFragment',
+    'MutationObserver',
+    'CSSStyleSheet',
+    'Text',
+  ];
+
+  test.each(globals)('has window.%s mapped to global', (globalName) => {
+    expect.assertions(3);
+    // @ts-expect-error - TODO: Fix index type.
+    expect(global[globalName]).toBeDefined();
+    // @ts-expect-error - TODO: Fix index type.
+    expect(window[globalName]).toBeDefined();
+    // @ts-expect-error - TODO: Fix index type.
+    expect(global[globalName]).toBe(window[globalName]);
+  });
+});
