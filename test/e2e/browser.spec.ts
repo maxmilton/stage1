758a62ad453c5c0a8e63ba3e86b7bb916a96cc45
Max Milton <max@happyhackers.xyz>
2025-04-17 20:55:47 +0900

    test: Add a basic e2e test

--- test/e2e/browser.spec.ts
+++ test/e2e/browser.spec.ts
@@ -1,2 +1,59 @@
-// TODO
-console.warn('Not implemented');
+import { resolve } from 'node:path';
+import { expect, test } from '@playwright/test';
+
+type Stage1 = typeof import('../../src/browser');
+
+declare global {
+  interface Window {
+    stage1: Stage1;
+  }
+}
+
+// const jsPath = Bun.resolveSync('../../dist/browser.js', '.');
+const jsPath = resolve(import.meta.dirname, '../../dist/browser.js');
+
+test('exports', async ({ page }) => {
+  // await page.goto('about:blank');
+  await page.addScriptTag({ path: jsPath });
+
+  const result = await page.evaluate(() => Object.keys(window.stage1));
+
+  expect(result).toEqual([
+    'append',
+    'clone',
+    'collect',
+    'create',
+    'deleteSyntheticEvent',
+    'fragment',
+    'h',
+    'html',
+    'insert',
+    'noop',
+    'onRemove',
+    'prepend',
+    'replace',
+    'setupSyntheticEvent',
+    'store',
+    'text',
+  ]);
+  expect(result).toHaveLength(16);
+});
+
+test('onRemove WIP', async ({ page }) => {
+  // await page.goto('about:blank');
+  await page.addScriptTag({ path: jsPath });
+
+  const result = await page.evaluate(async () => {
+    let calls = 0;
+
+    const root = document.createElement('div');
+    document.body.appendChild(root);
+    window.stage1.onRemove(root, () => calls++);
+    root.remove();
+
+    await Promise.resolve(); // wait for MutationObserver to trigger
+    return calls;
+  });
+
+  expect(result).toBe(1);
+});
