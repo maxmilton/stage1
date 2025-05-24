4fa2301912115124118ab0f17f47ffe49e2c7b28
Max Milton <max@happyhackers.xyz>
2025-05-24 19:37:47 +0900

    test: Fix e2e tests

--- test/e2e/browser.spec.ts
+++ test/e2e/browser.spec.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'node:path';
+import { resolve } from 'node:path'; // eslint-disable-line unicorn/import-style
 import { expect, test } from '@playwright/test';
 
 type Stage1 = typeof import('../../src/browser');
@@ -9,7 +9,6 @@ declare global {
   }
 }
 
-// const jsPath = Bun.resolveSync('../../dist/browser.js', '.');
 const jsPath = resolve(import.meta.dirname, '../../dist/browser.js');
 
 test('exports', async ({ page }) => {
@@ -29,31 +28,30 @@ test('exports', async ({ page }) => {
     'html',
     'insert',
     'noop',
-    'onRemove',
     'prepend',
     'replace',
     'setupSyntheticEvent',
     'store',
     'text',
   ]);
-  expect(result).toHaveLength(16);
+  expect(result).toHaveLength(15);
 });
 
-test('onRemove WIP', async ({ page }) => {
-  // await page.goto('about:blank');
-  await page.addScriptTag({ path: jsPath });
+// test('onRemove WIP', async ({ page }) => {
+//   // await page.goto('about:blank');
+//   await page.addScriptTag({ path: jsPath });
 
-  const result = await page.evaluate(async () => {
-    let calls = 0;
+//   const result = await page.evaluate(async () => {
+//     let calls = 0;
 
-    const root = document.createElement('div');
-    document.body.appendChild(root);
-    window.stage1.onRemove(root, () => calls++);
-    root.remove();
+//     const root = document.createElement('div');
+//     document.body.appendChild(root);
+//     window.stage1.onRemove(root, () => calls++);
+//     root.remove();
 
-    await Promise.resolve(); // wait for MutationObserver to trigger
-    return calls;
-  });
+//     await Promise.resolve(); // wait for MutationObserver to trigger
+//     return calls;
+//   });
 
-  expect(result).toBe(1);
-});
+//   expect(result).toBe(1);
+// });
