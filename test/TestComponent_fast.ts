1b2f207d6ac829eb7c2fdecd87c9a01680521a2a
Max Milton <max@happyhackers.xyz>
2025-06-23 10:42:44 +0900

    test: Add Test component for fast mode

---
+++ test/TestComponent_fast.ts
@@ -0,0 +1,28 @@
+import { collect, h } from '../src/fast/runtime.ts';
+import { compile } from '../src/macro.ts' with { type: 'macro' };
+
+type TestComponent = HTMLDivElement;
+
+interface TestProps {
+  text: string;
+}
+
+interface Refs {
+  t: Text;
+}
+
+const meta = compile<Refs>(`
+  <div id=test>
+    @t
+  </div>
+`);
+const view = h<HTMLDivElement>(meta.html);
+
+export function Test(props: TestProps): TestComponent {
+  const root = view;
+  const refs = collect<Refs>(root, meta.d);
+
+  refs[meta.ref.t].nodeValue = props.text;
+
+  return root;
+}
