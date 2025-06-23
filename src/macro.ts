0aedb68dd3b86a09f6b8509e4f9ced604960e740
Max Milton <max@happyhackers.xyz>
2025-06-23 10:41:43 +0900

    feat: Refactor macro; always allow comment refs, return success value, extra error checks, handle text in template root

--- src/macro.ts
+++ src/macro.ts
@@ -2,12 +2,6 @@ import type { IndicesOf, InferRefs, TupleOfKeys } from './types.ts';
 
 export interface CompileOptions {
   /**
-   * Whether to keep HTML comments in output HTML. When keepComments is true,
-   * comments can be used as refs.
-   * @default false
-   */
-  keepComments?: boolean;
-  /**
    * Whether to keep spaces adjacent to tags in output HTML. When keepSpaces
    * is false, `<div> x </div>` becomes `<div>x</div>`.
    * @default false
@@ -16,20 +10,26 @@ export interface CompileOptions {
 }
 
 /**
- * Bun macro which compiles a template string at build-time into a format that
- * can be used by the runtime.
+ * Bun macro to compile a template string at build-time into a format that can
+ * be used by the runtime.
  * @param template - HTML template string.
  * @param options - Compile options.
  */
 export function compile<R extends InferRefs<R> = object>(
   template: string,
-  { keepComments, keepSpaces }: CompileOptions = {},
+  { keepSpaces }: CompileOptions = {},
 ): {
   html: string;
+  /** Array of ref key names. */
   k: readonly string[];
+  /** Array of distances from previous ref node or template root. */
   d: readonly number[];
-  idx: IndicesOf<TupleOfKeys<R>>;
+  /** Object mapping ref key names to their indices in the `k` array. */
+  ref: IndicesOf<TupleOfKeys<R>>;
+  /** Whether the template was successfully compiled without any errors. */
+  success: boolean;
 } {
+  let success = true;
   const k: string[] = [];
   const d: number[] = [];
   let distance = 0;
@@ -38,23 +38,54 @@ export function compile<R extends InferRefs<R> = object>(
 
   const html = new HTMLRewriter()
     .onDocument({
+      doctype() {
+        // eslint-disable-next-line no-console
+        console.error(
+          'Found doctype but none was expected in template:',
+          template,
+        );
+        success = false;
+      },
       comments(node) {
-        if (keepComments) {
-          // TODO: Add documentation that precompiled mode supports using
-          // comments as refs. Requires the keepComments option to be true.
-          const text = node.text.trim();
+        const text = node.text.trim();
+        if (text[0] === '@') {
+          k.push(text.slice(1));
+          d.push(distance);
+          distance = 1;
+          // Replace with <!> which renders a Comment node at runtime
+          node.remove();
+          node.after('<!>', { html: true });
+        } else {
+          node.remove();
+        }
+      },
+      // This text handler is invoked twice for each Text node: first with the
+      // actual text, then with an empty last chunk. This behaviour stems from
+      // the fact that the data provided to `HTMLRewriter.transform()` can be
+      // streamed; where the last empty chunk signals the end of the text.
+      text(chunk) {
+        if (!chunk.lastInTextNode) {
+          const text = chunk.text.trim();
+          if (!text) {
+            if (!whitespaceSensitiveBlock) {
+              chunk.remove();
+            }
+            return;
+          }
           if (text[0] === '@') {
             k.push(text.slice(1));
             d.push(distance);
             distance = 0;
-            // TODO: Use `node.replace()` once lol-html fixes it for comments.
-            // node.replace('<!-->', { html: true });
-            node.remove();
-            node.after('<!-->', { html: true });
+            // Replace with single space which renders a Text node at runtime
+            chunk.replace(' ', { html: true });
+          } else if (!whitespaceSensitiveBlock) {
+            // Reduce any whitespace to a single space
+            chunk.replace(
+              (keepSpaces ? chunk.text : text).replace(/\s+/g, ' '),
+              { html: true },
+            );
           }
           distance++;
-        } else {
-          node.remove();
         }
       },
     })
@@ -72,6 +103,7 @@ export function compile<R extends InferRefs<R> = object>(
               'Expected template to have a single root element:',
               template,
             );
+            success = false;
           }
         }
 
@@ -92,48 +124,22 @@ export function compile<R extends InferRefs<R> = object>(
         }
         distance++;
       },
-      // This text handler is invoked twice for each Text node: first with the
-      // actual text, then with an empty last chunk. This behaviour stems from
-      // the fact that the data provided to `HTMLRewriter.transform()` can be
-      // streamed; where the last empty chunk signals the end of the text.
-      text(chunk) {
-        if (!chunk.lastInTextNode) {
-          const text = chunk.text.trim();
-          if (!text) {
-            if (!whitespaceSensitiveBlock) {
-              chunk.remove();
-            }
-            return;
-          }
-          if (text[0] === '@') {
-            k.push(text.slice(1));
-            d.push(distance);
-            distance = 0;
-            // replace with single space which renders a Text node at runtime
-            chunk.replace(' ', { html: true });
-          } else if (!whitespaceSensitiveBlock) {
-            // reduce any whitespace to a single space
-            chunk.replace(
-              (keepSpaces ? chunk.text : text).replace(/\s+/g, ' '),
-              { html: true },
-            );
-          }
-          distance++;
-        }
-      },
     })
     .transform(template.trim());
 
   // Check k entries are unique
   if (new Set(k).size !== k.length) {
-    throw new Error('Duplicate ref keys found in template');
+    // eslint-disable-next-line no-console
+    console.error('Duplicate ref keys found in template:', template);
+    success = false;
   }
 
   return {
     html,
     k,
     d,
-    // @ts-expect-error - TODO: Fix idx type
-    idx: Object.fromEntries(d.map((_, i) => [k[i], i])),
+    // @ts-expect-error - computed type
+    ref: Object.fromEntries(d.map((_, i) => [k[i], i])),
+    success,
   };
 }
