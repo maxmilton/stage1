19947cab5d7a09bd6c9d91621cab838df6707678
Max Milton <max@happyhackers.xyz>
2025-06-23 10:37:28 +0900

    feat: Reduce what's included in browser mode to reduce size

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,7 +1,12 @@
 // biome-ignore assist/source/organizeImports: manual sort order
 export * from './runtime.ts';
 
-export * from '../events.ts';
-export * from '../store.ts';
+export { ONCLICK, setupSyntheticClick } from '../events.ts';
 export * from '../types.ts';
-export * from '../utils.ts';
+export {
+  fragment,
+  text,
+  create,
+  clone,
+  append,
+} from '../utils.ts';
